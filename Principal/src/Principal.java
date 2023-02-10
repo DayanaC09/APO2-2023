36b9fcf8e2cb5c9cd37bf0f1c02171c4e5159736
Estudiante USC <estudiante@GTACAD.USC>
2023-02-10 15:55:03 -0500

    primer commit

---
+++ Principal/src/Principal.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+
+//nombre de la clase
+public class Principal {
+	
+	
+	//metodo de la clase
+	public static void main(String[] args) {
+		
+		
+		System.out.println("Ingrese la palabra");
+		
+		//clase que permite capturar datos por consola
+		Scanner sc = new Scanner(System.in);
+		
+		
+		String prueba = sc.next();
+		System.out.println("La cantidad de letras que tiene la palabra es :" + prueba.toUpperCase());
+	}
+}
